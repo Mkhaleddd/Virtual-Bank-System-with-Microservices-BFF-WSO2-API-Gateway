a39c5e27f9e8c1dd147fbaf76001febf5fac8a8e
janaYahyaa <jana7yehia@gmail.com>
2026-07-24 05:22:26 +0300

    feat: add user entity

---
+++ user-service/src/main/java/com/vbank/user_service/user/User.java
@@ -0,0 +1,39 @@
+package com.vbank.user_service.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    String username;
+
+    @Column(nullable = false, length = 100)
+    String firstName;
+
+    @Column(nullable = false, length = 100)
+    String lastName;
+
+    @Column(unique = true, nullable = false, length = 150)
+    String email;
+
+    @Column(nullable = false)
+    String passwordHash;
+}
