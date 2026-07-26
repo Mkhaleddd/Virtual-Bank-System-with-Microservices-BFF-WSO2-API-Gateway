b85dca3279542fa1819b93d279fb66933f4e3f7e
janaYahyaa <jana7yehia@gmail.com>
2026-07-26 21:34:37 +0300

    feat: add data to response structure

--- user-service/src/main/java/com/vbank/user_service/user/dto/RegisterResponse.java
+++ user-service/src/main/java/com/vbank/user_service/user/dto/RegisterResponse.java
@@ -1,13 +1,14 @@
 package com.vbank.user_service.user.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class RegisterResponse {
     private UUID userId;
     private String username;
--- user-service/src/main/java/com/vbank/user_service/user/dto/UserProfileResponse.java
+++ user-service/src/main/java/com/vbank/user_service/user/dto/UserProfileResponse.java
@@ -1,6 +1,7 @@
 package com.vbank.user_service.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class UserProfileResponse {
     private UUID userId;
     private String username;
